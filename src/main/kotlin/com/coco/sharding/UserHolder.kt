082bb85f8e95420c472153acafcca0f10a5086e8
DESKTOP-MJ25L1Q\chlrl <gisungcu@gmail.com>
2024-11-16 19:15:28 +0900

    thread local 추가

---
+++ src/main/kotlin/com/coco/sharding/UserHolder.kt
@@ -0,0 +1,39 @@
+package com.coco.sharding
+
+import com.coco.sharding.sharding.ShardingTarget
+
+class UserHolder {
+
+    companion object {
+        private val userContext = ThreadLocal<Context>()
+
+        fun setSharding(target: ShardingTarget, shardKey: Long) {
+            getUserContext().sharding = Sharding(target, shardKey)
+        }
+
+        fun clearSharding() {
+            getUserContext().sharding = null
+        }
+
+        fun getSharding(): Sharding? {
+            return getUserContext().sharding
+        }
+
+        private fun getUserContext(): Context {
+            return userContext.get()
+        }
+    }
+
+
+    class Context(
+        var sharding: Sharding?
+    ) {
+
+
+    }
+
+    class Sharding(
+        private val shardingTarget: ShardingTarget,
+        private val shardKey: Long
+    )
+}
